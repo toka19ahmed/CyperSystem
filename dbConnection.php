13cc9d0d4ea2e8e932453fd16a53d3a83501ce5a
toka19ahmed <56555180+toka19ahmed@users.noreply.github.com>
2019-10-15 03:32:55 +0200

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,9 +1,9 @@
 <?php
 
-    $dbhost = "localhost";
-    $dbuser = "root";
-    $dbpass = "";
-    $db = "cybersystem";
+    $dbhost = "l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbuser = "wymzk6tyd5kqemnn";
+    $dbpass = "i4v1vtal2e5476vm";
+    $db = "yqfgydu8tp2f3qyd";
     $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
     
 
